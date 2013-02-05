cf2045be4d5c78b7cd2b9b342db32f889ab11076
Yuki Tateoka <yukinko71@gmail.com>
2013-02-05 10:41:05 +0900

    [#43817821] Railsアプリをセット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleWebsite::Application.config.secret_token = 'daddb5b056cd095fd4ef798a462bf3175a1d866a10327801b17007ad3e56b9834f51013ce93009734195ad0016fffc7d0628fb9b052fb33ad009f8842501386b'
